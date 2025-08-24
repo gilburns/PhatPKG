5075808cc96d40ad73ea2728e16495de5aa6c0f9
Gil Burns <gil@gilburns.com>
2025-08-24 08:49:09 -0500

    Initial Commit

---
+++ PhatPKG/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PhatPKG
+//
+//  Created by Gil Burns on 8/24/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
